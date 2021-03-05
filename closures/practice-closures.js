8095affeea2dd23d8a2f895285ea60a5d85ad6e6
Esteban Portillo <portilloesteban1@gmail.com>
2021-03-05 14:34:25 -0400

    closures finisfhed

--- closures/practice-closures.js
+++ closures/practice-closures.js
@@ -9,12 +9,19 @@
   */
   
   //CODE HERE
-
+function myFunc (){
+  let myStr = 'super secret string'
+  function getSecretString(){
+    return myStr
+  }
+  return getSecretString
+ }
   
   //Now create a variable called secretString. Its value should be the invocation of myFunc.
 
   //CODE HERE
   
+  let secretString = myFunc()
   
   ////////////PROBLEM 2////////////
 
@@ -30,26 +37,41 @@
   
   //CODE HERE
 
+  function lightSwitch (){
+    let isTheLightOn = false
+
+    function flipTheSwitch(){
+      isTheLightOn = !isTheLightOn
+      if(isTheLightOn){
+        return('The light is on')
+      }
+      else{
+        return('The light is off')
+      }
+    }
+    return flipTheSwitch
+  }
   
   //Create a variable called kitchenSwitch whose value is the invocation of lightSwitch.
   
   //CODE HERE
-
+let kitchenSwitch = lightSwitch()
+console.log(kitchenSwitch())
   
   //Invoke kitchenSwitch.
 
   //CODE HERE
-  
+  kitchenSwitch
   
   //Create a variable called bathroomSwitch whose value is the invocation of lightSwitch. 
 
   //CODE HERE
   
-  
+  let bathroomSwitch = lightSwitch()
   //Invoke bathroomSwitch twice.
   
   //CODE HERE
-
+bathroomSwitch
   
   
   ////////////PROBLEM 3////////////
@@ -64,7 +86,22 @@
   */
 
   //CODE HERE
+function plantTracker(){
+  let plant = 'fern'
+  let height = 12
 
+  return{
+    readInfo : function(){
+      return (`This is a ${plant} plant that is ${height} inches tall.`)
+    },
+    waterPlant : function(){
+      return (height += 1) 
+    },
+    prunePlant : function (){
+      return (height -= 1)
+    }
+  }
+}
 
   ////////////PROBLEM 4////////////
 
@@ -80,13 +117,27 @@
 
   //CODE HERE
 
-
+  function inventory (){
+    let products = []
+    return({
+      readProducts : () => products,
+      addToProducts : (element) => products.push(element),
+      deleteFromProducts : (element) => {
+        for (let i = 0 ; i < products.length ; i++){
+          if (products[i] === element){
+            products.splice(i,1)
+          }
+        } 
+      }
+    })
+  }
   /*
     Create a variable called 'shoes' whose value is the invocation of inventory.
   */
 
   //CODE HERE
 
+  let shoes = inventory()
 
   /*
     Add an item to your shoes array using the addToProducts function
@@ -94,3 +145,4 @@
 
   //CODE HERE
 
+  shoes.addToProducts('clean as F***')
